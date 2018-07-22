e238732e7c31d90edeab5fcb74a0d2a1d32933bd
kyungjinMin <36812545+kyungjinMin@users.noreply.github.com>
2018-07-22 16:54:52 +0900

    민경진 : 랜덤 프로그래밍

---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class unknown
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As New Random
+        Dim b As New Integer
+
+        Label1.Text = a.Next(1, 15)
+        b = Label1.Text
+        If Label1.Text = b Then
+            Label2.Text = "★문제 번호" & b & " 이 뽑혔습니다★"
+        End If
+
+    End Sub
+
+    Private Sub RANDOM_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles 초기화.Click
+        Label2.Text = ""
+    End Sub
+End Class
